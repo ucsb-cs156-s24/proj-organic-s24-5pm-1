cf0090e892959514c5ea0570a496ab636822952c
Peak <pmanopchantaroj@ucsb.edu>
2024-05-28 19:53:08 -0700

    PM - added a staffcreatepage and will start on a staffForm now

---
+++ frontend/src/tests/pages/StaffCreatePage.test.js
@@ -0,0 +1,105 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import StaffCreatePage from "main/pages/StaffCreatePage";
+
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        useParams: () => ({
+            courseId: 1
+        }),
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
+
+describe("StaffCreatePage tests", () => {
+
+    const axiosMock = new AxiosMockAdapter(axios);
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    });
+
+    const queryClient = new QueryClient();
+    test("renders without crashing", () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <StaffCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+    test("on submit, makes request to backend", async () => {
+
+        const queryClient = new QueryClient();
+        const Staff = {
+            id: 1,
+            courseId: "2",
+            githubId: "3",
+        };
+
+        axiosMock.onPost(`/api/staff/post`).reply(202, Staff);
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <StaffCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => {
+            expect(screen.getByTestId("StaffForm-courseId")).toBeInTheDocument();
+        });
+
+        const courseField = screen.getByTestId("StaffForm-courseId");
+        const githubField = screen.getByTestId("StaffForm-githubId");
+        const submitButton = screen.getByTestId("StaffForm-submit");
+
+        fireEvent.change(courseField, { target: { value: 4 } });
+        fireEvent.change(githubField, { target: { value: 5 } });
+
+        expect(submitButton).toBeInTheDocument();
+
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+        console.log(axiosMock.history.post);
+
+        expect(axiosMock.history.post[0].params).toEqual(
+            {
+                "courseId": "4",
+                "githubId": "5"
+            });
+
+        expect(mockToast).toBeCalledWith("New staff added - id: 1");
+        expect(mockNavigate).toBeCalledWith({ "to": "/1/staff" });
+
+    });
+});
